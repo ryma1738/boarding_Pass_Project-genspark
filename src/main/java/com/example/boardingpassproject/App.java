0bd1234d5b44ed41306d9b5126831d33c5615ab3
Michael Albright <msalbrig@gmail.com>
2022-03-08 13:01:30 -0500

    text fields updated

--- src/main/java/com/example/boardingpassproject/App.java
+++ src/main/java/com/example/boardingpassproject/App.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("");
         stage.setScene(scene);
         stage.show();
     }
