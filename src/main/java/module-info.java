fc7c9de5a5081d5e75e1dd5e2a64f817fa4e2125
Brandon <Brandonthedev360@gmail.com>
2022-03-08 13:53:52 -0500

    minor fixes to code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.example.boardingpassproject {
     requires javafx.controls;
     requires javafx.fxml;
-    requires jfxrt;
-    requires rt;
 
 
     opens com.example.boardingpassproject to javafx.fxml;
